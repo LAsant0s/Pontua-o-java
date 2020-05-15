f9dcfab3ea9fa2e0b9489f0a6647ed46837ff534
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-15 19:17:31 -0300

    Cabecalho

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Impressão do cabeçalho
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		sc.close();
+
+	}
+
+}
