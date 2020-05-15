fdd11eed30bc69185d4b7063e7b05730cc3bc573
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-15 19:44:25 -0300

    Score de volume de compras

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,24 +9,41 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		//Impressão do cabeçalho
+		int pCompras, pInadimplencia, pForma;
+		
+		// Impressão do cabeçalho
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		System.out.println();
-		
-		//Leitura do volume e ticket médio
+
+		// Leitura do volume e ticket médio
 		System.out.print("Quantas compras o cliente fez no último ano? ");
-		int vCompras = sc.nextInt(); 
+		int vCompras = sc.nextInt();
 		System.out.print("Qual o ticket médio? ");
 		double ticket = sc.nextDouble();
 		System.out.println();
-		
-		//Leitura dos outros dados
+
+		// Leitura dos outros dados
 		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
-		int inadimplencia = sc.nextInt();  
+		int inadimplencia = sc.nextInt();
 		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
-		char fPagamento = sc.next().charAt(0); 
+		char fPagamento = sc.next().charAt(0);
+		System.out.println();
+		
+		// Score do volume de compras
+		if(vCompras == 0) {
+			pCompras = 0; 
+		}else if(vCompras > 2 && ticket <= 3000.00){
+			pCompras = 40; 
+		}else if(ticket <= 3000.00){
+				pCompras = 20; 	
+		}else {
+			pCompras = 60;
+		}
+		
+		System.out.println("Score de volume de compras = " + pCompras + " pontos");
+		
 		
 		sc.close();
 
