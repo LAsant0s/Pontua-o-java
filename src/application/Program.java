4ee5c702c4ef683930e23bcf27dc1d852cf71d14
Luan Assis <luan.assis05.santos@gmail.com>
2020-05-15 19:21:58 -0300

    Leituda do volume

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,13 @@ public class Program {
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println();
+		
+		//Leitura do volume e ticket médio
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		int vCompras = sc.nextInt(); 
+		System.out.print("Qual o ticket médio? ");
+		double ticket = sc.nextDouble();
 		
 		sc.close();
 
